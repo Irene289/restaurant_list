96470962ffd4a4b15ed444cd344f5e3610b5a876
Irene289 <esmuybonita18@gmail.com>
2022-01-22 18:49:51 +0800

    Fix search function bug

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 // app.js
 // require packages used in the project
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json').results
+const app = express()
 const port = 3000
-
-// require json data here
-const restaurantList = require('./restaurant.json')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -18,23 +16,23 @@ app.use(express.static('public'))
 // routes setting
 app.get('/', (req, res) => {
 
-  res.render('index', { restaurant: restaurantList.results })
+  res.render('index', { restaurant: restaurantList })
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant })
 })
 
 app.get('/search', (req, res) => {
   // console.log('req.query', req.query)
   // console.log('req.query.keyword', req.query.keyword)
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  const keyword = req.query.keyword.toLowerCase().trim()
+  const restaurants = restaurantList.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
   })
 
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+  res.render('index', { restaurant: restaurants, keyword })
 })
 
 // start and listen on the Express server
